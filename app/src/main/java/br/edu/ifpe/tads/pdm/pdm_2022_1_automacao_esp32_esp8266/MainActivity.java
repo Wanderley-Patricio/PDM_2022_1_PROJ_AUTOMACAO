a87da996d3128132d26d654e1d3ffe249c2a0d3b
Wanderley-Patricio <86197936+Wanderley-Patricio@users.noreply.github.com>
2022-04-16 17:34:16 -0300

    Primeiro commit

---
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pdm_2022_1_automacao_esp32_esp8266/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifpe.tads.pdm.pdm_2022_1_automacao_esp32_esp8266;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
